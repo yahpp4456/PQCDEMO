c55b212de7551df4c86ba1a3a4aaec967db85a09
SAM <SAM@DESKTOP-Q1G9JKC>
2023-11-15 10:19:39 +0800

    加入專案檔案。

---
+++ PQCDEMO/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace PQCDEMO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            groupBox1 = new GroupBox();
+            pictureBox1 = new PictureBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1160, 308);
+            button1.Name = "button1";
+            button1.Size = new Size(310, 167);
+            button1.TabIndex = 0;
+            button1.Text = "Test";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1464, 260);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Right;
+            pictureBox1.Image = Properties.Resources._656;
+            pictureBox1.Location = new Point(1148, 23);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(313, 234);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1482, 827);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private GroupBox groupBox1;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ PQCDEMO/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System.Diagnostics;
+using TPM;
+
+namespace PQCDEMO
+{
+
+
+
+    public partial class Form1 : Form
+    {
+
+        TextBox[] textBoxes = new TextBox[16];
+        UInt16 AxisOrgStatus;
+        Master.PCI_M114.ErrCode ret;
+        ushort SwitchNo;
+
+        Dictionary<int, string> bitMeaningMapping = new Dictionary<int, string>
+                {
+                    { 0, "RDY" },
+                    { 1, "ALM " },
+                    { 2, "+EL" },
+                    { 3, "-EL" },
+                    { 4, "ORG" },
+                    { 5, "DIR" },
+                    { 6, "EMG " },
+                    { 7, "Reserved" },
+                    { 8, "ERC" },
+                    { 9, "EZ " },
+                    { 10, "Reserved" },
+                    { 11, "Latch" },
+                    { 12, "SD" },
+                    { 13, "INP" },
+                    { 14, "SVON" },
+                    { 15, "RALM" }
+                 };
+
+        bool IsFunctionActive(string functionName, int statusWord)
+        {
+            var bitPosition = bitMeaningMapping.FirstOrDefault(x => x.Value == functionName).Key;
+            return (statusWord & (1 << bitPosition)) != 0;
+        }
+
+
+
+
+        private void UpdateTextBoxesAndColors(UInt16 axis, UInt16 axisStatus)
+        {
+            List<TextBox> textBoxGroup = null;
+            string axisName = "";
+
+            switch (axis)
+            {
+                case 0:
+                    textBoxGroup = axis1TextBoxes;
+                    axisName = "X";
+                    break;
+                case 1:
+                    textBoxGroup = axis2TextBoxes;
+                    axisName = "Y";
+                    break;
+                case 2:
+                    textBoxGroup = axis3TextBoxes;
+                    axisName = "Z";
+                    break;
+                default:
+                    // Handle invalid axis value if needed
+                    break;
+            }
+
+            if (textBoxGroup != null)
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    TextBox textBox = textBoxGroup[i];
+                    string functionName = bitMeaningMapping[i];
+
+                    // Set the TextBox text to the corresponding function name
+                    textBox.Text = axisName + " " + functionName;
+
+                    // Check if the function is active (IsFunctionActive result is 1)
+                    bool isActive = IsFunctionActive(functionName, axisStatus);
+
+                    // Change TextBox background color based on the IsActive status
+                    textBox.BackColor = isActive ? Color.LightGreen : Color.Green;
+                }
+            }
+        }
+
+
+        // 創建三個不同的列表，每個列表代表一組軸的文本框
+        private List<TextBox> axis1TextBoxes = new List<TextBox>();
+        private List<TextBox> axis2TextBoxes = new List<TextBox>();
+        private List<TextBox> axis3TextBoxes = new List<TextBox>();
+        public Form1()
+        {
+            InitializeComponent();
+            initMot();
+
+            InitializeTextBoxGroup(axis1TextBoxes, groupBox1, 50, "X");
+            InitializeTextBoxGroup(axis2TextBoxes, groupBox1, 100, "Y");
+            InitializeTextBoxGroup(axis3TextBoxes, groupBox1, 150, "Z");
+        }
+        private void InitializeTextBoxGroup(List<TextBox> textBoxGroup, GroupBox groupBox, int startY, string axisName)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                TextBox textBox = new TextBox();
+                textBox.Parent = groupBox; // 設置文本框的父容器為groupBox
+                textBox.Location = new System.Drawing.Point((i + 1) * 65 - 30, startY);
+                textBox.Size = new System.Drawing.Size(65, 30); // 調整文本框的寬度為50
+
+                // 為文本框指定名稱，格式為 axisName + "info" + i
+                string v = axisName + "info" + i;
+                string textBoxName = v;
+                textBox.Name = textBoxName;
+                textBox.Text = v;
+                textBox.BackColor = Color.LightGreen;
+                // 設置 TextBox 為只讀模式
+                textBox.ReadOnly = true;
+                textBox.Enabled = false;
+                textBox.TextAlign = HorizontalAlignment.Center;
+                textBox.Cursor = Cursors.Arrow;
+                // 將文本框添加到textBoxGroup中
+                textBoxGroup.Add(textBox);
+            }
+        }
+        private void initMot()
+        {
+
+
+            UInt16 existcards = 0;
+            UInt16 CardNo = 0; // 
+
+
+            ret = Master.PCI_M114._m114_open(ref existcards);
+            if (existcards == 0 || ret != Master.PCI_M114.ErrCode.ERR_NoError)
+            {
+                MessageBox.Show("No any PCI_M114 or error in opening card!", "Error");
+                return;
+            }
+            else { MessageBox.Show("existcards: " + existcards); }
+
+
+            ret = Master.PCI_M114._m114_get_switch_card_num(CardNo, ref SwitchNo);
+            if (ret != Master.PCI_M114.ErrCode.ERR_NoError)
+            {
+                MessageBox.Show("Error in getting switch card number!", "Error");
+                return;
+            }
+
+            // 初始化控制卡
+            ret = Master.PCI_M114._m114_initial(SwitchNo);
+            if (ret != Master.PCI_M114.ErrCode.ERR_NoError)
+            {
+                MessageBox.Show("Error in initializing card!", "Error");
+                return;
+            }
+            else { MessageBox.Show("SwitchNo: " + SwitchNo); }
+
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            for (UInt16 axis = 0; axis < 3; axis++)
+            {
+                ret = Master.PCI_M114._m114_get_io_status(SwitchNo, axis, ref AxisOrgStatus);
+                if (ret == Master.PCI_M114.ErrCode.ERR_NoError)
+                {
+
+                    UpdateTextBoxesAndColors(axis, AxisOrgStatus);
+                }
+                else
+                {
+
+                }
+            }
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
