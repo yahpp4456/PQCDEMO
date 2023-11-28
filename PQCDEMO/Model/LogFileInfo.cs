b3016037323d8d3391f6111f92b81eb162e21469
unknown <wilson.chen@yjtech.tw>
2023-11-28 14:43:02 +0800

    add

---
+++ PQCDEMO/Model/LogFileInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PQCDEMO.Model
+{
+    public class LogFileInfo
+    {
+       public string fileName { get; set; }
+        public string targetUrl { get; set; }
+    }
+}
